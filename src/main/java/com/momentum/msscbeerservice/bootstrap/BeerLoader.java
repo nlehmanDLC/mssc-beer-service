3f364a0da4a2f72518db8808de94780fd8154c67
Nathan Lehman <nlehman@dlcouch.com>
2021-01-25 11:19:59 -0500

    adding bootstrap. closes #7

---
+++ src/main/java/com/momentum/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,47 @@
+package com.momentum.msscbeerservice.bootstrap;
+
+import com.momentum.msscbeerservice.domain.Beer;
+import com.momentum.msscbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if(beerRepository.count() == 0) {
+            beerRepository.save(Beer
+                    .builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(231225351233L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer
+                    .builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(2654684561233L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+    }
+}
