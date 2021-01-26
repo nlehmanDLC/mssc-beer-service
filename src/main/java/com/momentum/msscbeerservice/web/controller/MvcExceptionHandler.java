bba402e5e64c291dcc100619cdb6f28b48ff5f19
Nathan Lehman <nlehman@dlcouch.com>
2021-01-26 09:18:46 -0500

    updated tests for validation rules.

--- src/main/java/com/momentum/msscbeerservice/web/controller/MvcExceptionHandler.java
+++ src/main/java/com/momentum/msscbeerservice/web/controller/MvcExceptionHandler.java
@@ -2,15 +2,39 @@ package com.momentum.msscbeerservice.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class MvcExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List> validationErrorHandler(MethodArgumentNotValidException e) {
+        List<String> errors = new ArrayList<>();
+        List<FieldError> fieldErrors = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(FieldError.class::cast)
+                .collect(Collectors.toList());
+        fieldErrors.forEach(fieldError -> {
+            errors.add(String.format(
+                    "Bad Request %s: %s: Rejected value: ---> %s",
+                    fieldError.getField(),
+                    fieldError.getDefaultMessage(),
+                    fieldError.getRejectedValue()
+            ));
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<List> validationErrorHandler(ConstraintViolationException e) {
@@ -19,5 +43,10 @@ public class MvcExceptionHandler {
         e.getConstraintViolations().forEach(error -> errorList.add(error.toString()));
 
         return new ResponseEntity<>(errorList, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<List> handleBindException(BindException ex) {
+        return new ResponseEntity(ex.getAllErrors(), HttpStatus.BAD_REQUEST);
     }
 }
--- src/test/java/com/momentum/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/momentum/msscbeerservice/web/controller/BeerControllerTest.java
@@ -2,12 +2,15 @@ package com.momentum.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.momentum.msscbeerservice.web.model.BeerDto;
+import com.momentum.msscbeerservice.web.model.BeerStyleEnum;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -22,6 +25,19 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    private BeerDto beerDto;
+
+    @BeforeEach
+    void setUp() {
+        beerDto = BeerDto
+                .builder()
+                .beerName("Corona")
+                .beerStyle(BeerStyleEnum.ALE)
+                .upc(123456789012L)
+                .price(new BigDecimal("12.95"))
+                .build();
+    }
+
     @Test
     void getBeerById() throws Exception {
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
@@ -30,7 +46,6 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(
@@ -42,7 +57,6 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(
