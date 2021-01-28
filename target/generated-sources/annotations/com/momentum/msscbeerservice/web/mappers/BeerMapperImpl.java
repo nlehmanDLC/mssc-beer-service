a2bd142348bbfa94d6edcacdbf19ec27c481f05d
Nathan Lehman <nlehman@dlcouch.com>
2021-01-28 16:52:02 -0500

    use parent bom

--- target/generated-sources/annotations/com/momentum/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/momentum/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-28T09:21:10-0500",
-    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 15.0.1 (Oracle Corporation)"
+    date = "2021-01-28T16:50:46-0500",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.1 (Oracle Corporation)"
 )
 @Component
 @Primary
