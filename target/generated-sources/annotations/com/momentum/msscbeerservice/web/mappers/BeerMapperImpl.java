21f74dc77cc6ff70bf30afb076b961d73135df71
Nathan Lehman <nlehman@dlcouch.com>
2021-01-29 14:52:21 -0500

    update BOM version

--- target/generated-sources/annotations/com/momentum/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/com/momentum/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-29T14:25:29-0500",
+    date = "2021-01-29T14:52:07-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.10 (Amazon.com Inc.)"
 )
 @Component
